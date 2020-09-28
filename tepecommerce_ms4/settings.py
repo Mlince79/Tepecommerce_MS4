6d8400885eefc6f07b33e485c1d9b866e346178d
Marisol <marisol.lince@gmail.com>
2020-09-28 08:07:31 +0000

    Added profiles app

--- tepecommerce_ms4/settings.py
+++ tepecommerce_ms4/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'checkout',
     'about',
     'contactemail',
+    'profiles',
 
     # Other
     'crispy_forms',
--- tepecommerce_ms4/urls.py
+++ tepecommerce_ms4/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
     path('about/', include('about.urls')),
     path('contactemail/', include('contactemail.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
