50f8296eb8e1181c07299ec4e098f0cf34425f45
Marisol <marisol.lince@gmail.com>
2020-10-21 09:21:16 +0000

    Added cache control

--- tepecommerce_ms4/settings.py
+++ tepecommerce_ms4/settings.py
@@ -13,6 +13,7 @@ import os
 import dj_database_url
 from pathlib import Path
 
+
 if os.path.exists("env.py"):
     import env
 
@@ -197,6 +198,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'ecommercetepe'
     AWS_S3_REGION_NAME = 'eu-north-1'
