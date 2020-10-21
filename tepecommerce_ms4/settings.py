ecfe760cc0c72525e9330eb1c8e70359dedc7ead
Marisol <marisol.lince@gmail.com>
2020-10-21 08:54:53 +0000

    Changed settings.py

--- tepecommerce_ms4/settings.py
+++ tepecommerce_ms4/settings.py
@@ -186,9 +186,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'tepecommerce_ms4/static')
-]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
